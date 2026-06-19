d140ae6b82b3fb643b7faca7c49acb9245009d19
Snowflake200922 <2849137191@qq.com>
2026-06-19 18:48:20 +0800

    添加 start.bat

---
+++ start.bat
@@ -0,0 +1,6 @@
+﻿@echo off
+cd /d "%~dp0"
+echo 论坛已启动：http://localhost:3000
+echo 按 Ctrl+C 关闭
+echo.
+"C:\Users\28491\.cache\codex-runtimes\codex-primary-runtime\dependencies\node\bin\node.exe" server.js
